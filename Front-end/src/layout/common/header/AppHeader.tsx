601aa5ebee8fe6016a8bba10539980e8cb1d0b37
João Oliveira <joaooliveira1999@outlook.com>
2024-08-20 16:20:17 -0300

    style: adjusted Header

--- Front-end/src/layout/common/header/AppHeader.tsx
+++ Front-end/src/layout/common/header/AppHeader.tsx
@@ -13,14 +13,7 @@ export const AppHeader = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/projects">
-              Projetos
-            </Nav.Link>
-            <Nav.Link as={Link} to="/tasks">
-              Tarefas
-            </Nav.Link>
-          </Nav>
+          <Nav className="me-auto"></Nav>
           <LoginModal />
         </Navbar.Collapse>
       </Container>
